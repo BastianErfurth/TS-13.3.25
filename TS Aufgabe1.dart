5b4faed7c67f74b9408497ef5095378ec9a71f35
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-13 10:44:30 +0100

    Aufgabe 1 TS  gelöst

---
+++ TS Aufgabe1.dart
@@ -0,0 +1,16 @@
+int sumOfTwoNumbers(int number1, int number2) {
+  return number1 + number2;
+}
+
+void main() {
+  sumOfTwoNumbers(2, 6);
+  sumOfTwoNumbers(17, 23);
+  sumOfTwoNumbers(112, 88);
+
+  int result1 = sumOfTwoNumbers(2, 6);
+  print(result1);
+  int result2 = sumOfTwoNumbers(17, 23);
+  print(result2);
+  int result3 = sumOfTwoNumbers(112, 88);
+  print(result3);
+}
