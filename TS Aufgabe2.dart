e73a34ed09ae073823b4c24b2107a609e581cdd6
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-13 10:50:25 +0100

    Aufgabe 2 gelöst

---
+++ TS Aufgabe2.dart
@@ -0,0 +1,9 @@
+int numberOfTextCharakters(String text) {
+  return text.length;
+}
+
+void main() {
+  print(numberOfTextCharakters("FunktionenII"));
+  print(numberOfTextCharakters("es ist nicht so einfach"));
+  print(numberOfTextCharakters("wird schon"));
+}
